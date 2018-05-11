21f97cea83d92278bc157734a7f9ad831f11c51f
Payal Mavani <38760896+payalmavani007@users.noreply.github.com>
2018-05-11 22:52:19 +0530

    Initial commit

---
+++ app/src/main/java/trailer/movie/pro/sau/movietrailer/SplashScreen.java
@@ -0,0 +1,13 @@
+package trailer.movie.pro.sau.movietrailer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
